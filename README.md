8df7fe22eac712063943515740f2259185f53a4a
user_123 <11405033+user-123@users.noreply.github.com>
2025-08-08 22:42:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Java Tool Collections（Java 常用工具集合）
+
+本專案為一套封裝常用 Java 工具類與函式的集合，旨在提升開發效率，統一工具使用風格，"預計"涵蓋以下功能模組：：
+
+- 字串處理（StringUtils）
+- 時間處理（DateTimeUtils）
+- 集合處理（CollectionUtils）
+- 檔案與I/O操作（FileIOUtils）
+- 加密/編碼工具（CryptoUtils）
+- 反射與泛型工具（ReflectionUtils）
+- 錯誤處理與斷言（AssertUtils）
+
+---
+
+## 開發環境需求
+- Java JDK 版本：21
+- 建構工具：Gradle 8.14.3
+- 模組系統：Java Platform Module System（JPMS，模組名：org.dc.javatools）
+
+---
+
+## 如何使用
+//TODO
+
+---
+
+## 工具類簡介
+//TODO
+
+---
+
+## 授權條款
+
+本專案採用 MIT 授權條款，僅供內部使用或教育學習，禁止未經授權之商業散佈。
---
+++ build.gradle.kts
@@ -0,0 +1,179 @@
+plugins {
+    java
+}
+
+group = "org.dc"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+        implementation.set(JvmImplementation.VENDOR_SPECIFIC)
+    }
+}
+
+gradle.taskGraph.whenReady {
+    println("==> Gradle is running with: ${System.getProperty("java.home")}")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(21)
+    options.encoding = "UTF-8"
+
+    doFirst {
+        println("===== JavaCompile Info =====")
+        println("Compiler path            : ${javaCompiler.get().metadata.installationPath}")
+        println("Source compatibility     : $sourceCompatibility")
+        println("Target compatibility     : $targetCompatibility")
+        println("Release option           : ${options.release.orNull ?: "N/A"}")
+        println("Boot classpath           : ${options.bootstrapClasspath?.files ?: "N/A"}")
+        println("Source path              : ${options.sourcepath?.files ?: "N/A"}")
+        println("Annotation processor path: ${options.annotationProcessorPath?.files ?: "N/A"}")
+        println("============================")
+    }
+}
+
+
+// 將依賴複製到指定目錄
+tasks.register<Copy>("copyDependencies") {
+    from(configurations.runtimeClasspath)
+    into(layout.buildDirectory.dir("libs/lib"))
+}
+
+// Dry JAR（不含依賴）
+tasks.register<Jar>("jar_DryJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "${project.group}.RunEncryptor"
+    }
+    from(sourceSets.main.get().output)
+    //archiveFileName.set("${project.name}-dry.jar")
+}
+
+// Thin JAR（只含主程式與外部依賴路徑）
+tasks.register<Jar>("jar_ThinJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes(
+            "Main-Class" to "${project.group}.RunEncryptor",
+            "Class-Path" to configurations.runtimeClasspath.get().files.joinToString(" ") { "lib/${it.name}" }
+        )
+    }
+    dependsOn("copyDependencies")
+    from(sourceSets.main.get().output)
+    //archiveFileName.set("${project.name}-thin.jar")
+}
+
+// Fat JAR（含所有依賴）
+tasks.register<Jar>("jar_FatJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "${project.group}.Main"
+        //attributes["Premain-Class"] = "${project.group}.util.component.LogInitializer"
+    }
+    from({
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
+    //archiveFileName.set("${project.name}-fat.jar")
+}
+
+// TODO 解決 build.gradle 內建的 jar 會繼續執行或循環引用的問題
+// val jarType = project.findProperty("jarType")?.toString() ?: "fatJar"
+//
+// if (jarType == "fatJar") {
+//     println("📦 Building FAT JAR")
+//     dependsOn("jar_FatJar")
+//     finalizedBy("jar_FatJar")
+// } else if (jarType == "thinJar") {
+//     println("📦 Building THIN JAR")
+//     dependsOn("jar_ThinJar")
+//     finalizedBy("jar_ThinJar")
+// }
+
+// 若是能修復 build.gradle 內建的 jar 會繼續執行或循環引用的問題，移除以下 task 代碼
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "${project.group}.RunEncryptor"
+    }
+    from(sourceSets.main.get().output)
+    //archiveFileName.set("${project.name}-dry.jar")
+}
+
+// 印出完整依賴樹
+tasks.register("printDependencyTree") {
+    doLast {
+        val config = configurations.runtimeClasspath.get()
+        val resolved = config.resolvedConfiguration
+        println("Dependency tree for configuration: ${config.name}")
+        val visited = mutableSetOf<String>()
+
+        fun printDependency(dep: ResolvedDependency, indent: String = "") {
+            val depId = "${dep.moduleGroup}:${dep.moduleName}:${dep.moduleVersion}"
+            println("$indent+-- $depId")
+
+            if (!visited.add(depId)) {
+                println("$indent    (already printed)")
+                return
+            }
+
+            dep.children.forEach { child ->
+                printDependency(child, "$indent    ")
+            }
+        }
+
+        resolved.firstLevelModuleDependencies.forEach {
+            printDependency(it)
+        }
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    google()
+    gradlePluginPortal()
+}
+
+
+
+val springVersion = "6.2.9"
+val lombokVersion = "1.18.38"
+val mapstructVersion = "1.6.3"
+val lombokMapstructBindingVersion = "0.2.0"
+
+
+
+dependencies {
+    implementation("org.springframework:spring-context:$springVersion")
+    implementation("org.springframework:spring-jdbc:$springVersion")
+    implementation("org.springframework:spring-tx:$springVersion")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+    implementation("org.hibernate.validator:hibernate-validator:9.0.1.Final")
+
+    runtimeOnly("com.mysql:mysql-connector-j:9.3.0")
+
+    compileOnly("org.mapstruct:mapstruct:$mapstructVersion")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    implementation("org.projectlombok:lombok-mapstruct-binding:$lombokMapstructBindingVersion")
+
+    implementation("com.google.guava:guava:33.4.8-jre")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.13.4")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
