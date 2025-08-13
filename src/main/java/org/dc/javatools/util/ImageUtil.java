5eeaed11d49bfa804e8a8c883f999e409b854e59
user_123 <11405033+user-123@users.noreply.github.com>
2025-08-14 03:35:37 +0800

    feat: 新增ImageUtil

---
+++ src/main/java/org/dc/javatools/util/ImageUtil.java
@@ -0,0 +1,155 @@
+package org.dc.javatools.util;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+@Slf4j
+public class ImageUtil {
+
+	private ImageUtil() {throw new IllegalStateException("Utility class");}
+
+	@Deprecated
+	public static boolean resizeImageToJpg(String inputPath, String outputPath, int targetWidth, int targetHeight) {
+		return resizeImage(inputPath, outputPath, targetWidth, targetHeight, "jpg");
+	}
+
+	public static boolean resizeImage(String inputPath, String outputPath, int targetWidth, int targetHeight) {
+		String originalFormat = FileUtil.getFileNameExtension(FileUtil.readFile(inputPath));
+		return !StringUtil.isNullOrBlank(originalFormat) && resizeImage(inputPath, outputPath, targetWidth, targetHeight, originalFormat);
+	}
+
+	public static boolean resizeImage(MultipartFile file, String outputPath, int targetWidth, int targetHeight) {
+		String originalFormat = FileUtil.getFileNameExtension(file);
+		return !StringUtil.isNullOrBlank(originalFormat) && resizeImage(file, outputPath, targetWidth, targetHeight, originalFormat);
+	}
+
+	public static boolean resizeImage(String inputPath, String outputPath, int targetWidth, int targetHeight, String imgFormat) {
+		if(isIllegalResolution(targetWidth, targetHeight)) {return false;}
+		//讀取檔案
+		BufferedImage originalImage = readImage(inputPath);
+		return resizeImage(originalImage, outputPath, targetWidth, targetHeight, imgFormat);
+	}
+
+	public static boolean resizeImage(MultipartFile file, String outputPath, int targetWidth, int targetHeight, String imgFormat) {
+		if(isIllegalResolution(targetWidth, targetHeight)) {return false;}
+		//讀取檔案
+		BufferedImage originalImage = readImage(file);
+		return resizeImage(originalImage, outputPath, targetWidth, targetHeight, imgFormat);
+	}
+
+	public static boolean resizeImage(BufferedImage originalImage, String outputPath, int targetWidth, int targetHeight, String imgFormat) {
+		if(isNullOrEmpty(originalImage)) {return false;}
+		//檢查原圖大小與目標大小
+		if(isSameSize(originalImage, targetWidth, targetHeight)) {
+			log.debug("圖片原始解析度與設定值一致，不進行resize");
+			return true;
+		}
+
+		//渲染圖片
+		BufferedImage targetImage = new BufferedImage(targetWidth, targetHeight, originalImage.getType());
+		Graphics2D graphics = targetImage.createGraphics();
+		graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		boolean renderResult = graphics.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+		//檢查渲染程序是否成功啟動
+		if(!renderResult) {
+			log.error("啟動圖像繪製，{}", "啟動失敗");
+			return false;
+		}
+
+		//寫入檔案
+		boolean writeResult = false;
+		try {
+			writeResult = ImageIO.write(targetImage, imgFormat, Paths.get(outputPath).toFile());
+			log.debug("寫入圖片檔案，{}", "寫入成功");
+		} catch (IOException e) {
+			log.error("寫入圖片檔案，{}", "寫入錯誤");
+		}
+		return writeResult;
+	}
+
+	public static BufferedImage readImage(MultipartFile multipartFile) {
+		byte[] bytes = FileUtil.parseBytes(multipartFile);
+		BufferedImage bufferedImage = null;
+		try (InputStream inputStream = new ByteArrayInputStream(bytes)) {
+			bufferedImage = ImageIO.read(inputStream);
+			log.debug("讀取圖片檔案，{}", "讀取成功");
+		} catch (IOException ex) {
+			log.error("讀取圖片檔案，{}", "讀取失敗", ex);
+		}
+		return bufferedImage;
+	}
+
+	public static BufferedImage readImage(InputStream inputStream) {
+		BufferedImage bufferedImage = null;
+		try {
+			bufferedImage = ImageIO.read(inputStream);
+			log.debug("讀取圖片檔案，{}", "讀取成功");
+		} catch (IOException ex) {
+			log.error("讀取圖片檔案，{}", "讀取失敗", ex);
+			log.debug("{}", new Object() {}.getClass().getEnclosingMethod().getName(), ex);
+		}
+		return bufferedImage;
+	}
+
+	public static BufferedImage readImage(String inputPath) {
+		BufferedImage bufferedImage = null;
+		try {
+			bufferedImage = ImageIO.read(FileUtil.toFile(inputPath));
+			log.debug("讀取圖片檔案，{}", "讀取成功");
+		} catch (IOException ex) {
+			log.error("讀取圖片檔案，{}", "讀取失敗", ex);
+		}
+		return bufferedImage;
+	}
+
+	private static boolean isNullOrEmpty(BufferedImage bufferedImage) {
+		return bufferedImage==null || bufferedImage.getWidth()==0 || bufferedImage.getHeight()==0;
+	}
+
+	private static boolean isIllegalResolution(int width, int height) {
+		return width<0 || height<0;
+	}
+
+	private static boolean isSameSize(BufferedImage image, int targetWidth, int targetHeight) {
+		return image.getWidth()==targetWidth && image.getHeight()==targetHeight;
+	}
+
+	public static boolean checkImageSize(MultipartFile file, int targetWidth, int targetHeight, COMPARISON_OPTION comparisonOption) {
+		return checkImageSize(readImage(file), targetWidth, targetHeight, comparisonOption);
+	}
+
+	public static boolean checkImageSize(InputStream inputStream, int targetWidth, int targetHeight, COMPARISON_OPTION comparisonOption) {
+		return checkImageSize(readImage(inputStream), targetWidth, targetHeight, comparisonOption);
+	}
+
+    @Accessors(fluent = true)
+    @ToString
+    @AllArgsConstructor(access = AccessLevel.PACKAGE)
+	public enum COMPARISON_OPTION {
+		GREATER(1), EQUAL(0), LESSER(-1);
+		private int option;
+		protected int compare() {return this.option;}
+	}
+
+	public static boolean checkImageSize(BufferedImage image, int targetWidth, int targetHeight, COMPARISON_OPTION comparisonOption) {
+		return !isNullOrEmpty(image) && !isIllegalResolution(targetWidth, targetHeight) && comparisonOption!=null && switch (comparisonOption.compare()) {
+			case 0 -> isSameSize(image, targetWidth, targetHeight);
+			case 1 -> image.getWidth()>targetWidth && image.getHeight()>targetHeight;
+			case -1 -> image.getWidth()<targetWidth && image.getHeight()<targetHeight;
+			default -> throw new IllegalStateException("Unexpected value: " + comparisonOption.compare());
+		};
+	}
+}
